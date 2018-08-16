28ab24e9996fec0a47c9d3ee33ccb1661e8ef039
JallenKwong <jallenkwong@163.com>
2018-08-17 00:02:10 +0800

    write python script to update README.md

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # EASY #
 
-
 # MEDIUM #
 
-- [Number of IslandsNumber of Islands](src/main/resources/com/lun/medium/NumberOfIslands.md)
+- [NumberOfIslands](src/main/java/com/lun/medium/NumberOfIslands.md) --- [Solution Code](src/main/java/com/lun/medium/NumberOfIslands.java)
+
+- [RectangleArea](src/main/java/com/lun/medium/RectangleArea.md) --- [Solution Code](src/main/java/com/lun/medium/RectangleArea.java)
 
 # HARD #
 
-- [Find Median from Data Stream](src/main/resources/com/lun/hard/FindMedianFromDataStream.md)
+- [FindMedianFromDataStream](src/main/java/com/lun/hard/FindMedianFromDataStream.md) --- [Solution Code](src/main/java/com/lun/hard/FindMedianFromDataStream.java)
+
---
+++ UpdateREADMEFile.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*
+
+#将已完成的题目索引制造到本目录的README.md
+
+import os
+
+targetFile = open("README.md","w")
+
+# 读取 src\main\java\com\lun 的文件名 EASY,MEDIUM,HARD 作大标题
+
+srcDir = "src\\main\\java\\com\\lun\\"
+
+#dirs = os.listdir(srcDir)
+
+dirs = ['easy','medium','hard']
+
+for d in dirs:
+
+    #print d
+    
+    targetFile.write("# " + d.upper() + " #\n\n")
+
+    dirs2 = os.listdir(srcDir + d)
+
+    for d2 in dirs2:
+
+        if d2.endswith('.md'):
+            fileName = d2.split('.')
+
+            dir1 = (srcDir + d + "\\").replace("\\","/")
+            
+            line = "- [" + fileName[0] + "](" + dir1 + d2 + \
+                ") --- [Solution Code]("+ dir1 + fileName[0]  + ".java)\n\n"
+            targetFile.write(line)
+
+targetFile.close()
+
+print "Update README.md successfully! Press Enter to exit."
+
+
+try:
+    input()
+except:
+    print "Bye!"
+    
